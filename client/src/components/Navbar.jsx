d4350d9e5e01304c5bc15b6105ef2109f193d861
Soham Kolhatkar <sohamk130405@gmail.com>
2024-12-19 09:28:43 +0530

    bug fix

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = () => {
     };
 
     user && fetchOrgs();
-  }, []);
+  }, [user]);
   return (
     <div className="md:w-[calc(100%-230px)] w-[calc(100%-60px)] fixed flex items-center justify-between pl-2 pr-6 h-[70px] top-0 md:left-[230px] left-[60px] border-b border-slate-300 bg-[#fff]">
       {user && (
